595ca55b21f26a4f1ac821156f15ab50b3bac1e4
李乐 <lile@boimc.com>
2023-12-11 17:02:48 +0800

    reset info

---
+++ ds_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+SERVER_EP = 'http://localhost:12345'
+
+DB_HOST = 'IP'
+DB_PORT = 5432
+DB_DATABASE = 'dolphinscheduler'
+DB_USER = 'dolphinscheduler'
+DB_PASSWORD = 'dolphinscheduler'
+DB_CLIENT_ENCODING = 'utf-8'
+DB_SSL_MODE = 'disable'
